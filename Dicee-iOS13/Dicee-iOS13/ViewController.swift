75cdfd2557cbdec6649aaaf0556b4aa3fe7c7d7e
aniket19233-maker <73880538+aniket19233-maker@users.noreply.github.com>
2021-08-07 01:28:27 +0530

    Add files via upload

---
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    var leftDiceNumber = 1;
+    var rightDiceNumber = 5;
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne") ,  #imageLiteral(resourceName: "DiceTwo") ,  #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix")  ][leftDiceNumber]
+        leftDiceNumber = (leftDiceNumber + 1)%6
+        diceImageView2.image = [ #imageLiteral(resourceName: "DiceOne") ,  #imageLiteral(resourceName: "DiceTwo") ,  #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix")  ][rightDiceNumber]
+        rightDiceNumber = (rightDiceNumber-1+6)%6
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
