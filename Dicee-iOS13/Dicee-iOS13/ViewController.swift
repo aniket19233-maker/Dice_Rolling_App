5fe50655f463ecf86523fa394adbbb4fa139ff04
aniket19233-maker <73880538+aniket19233-maker@users.noreply.github.com>
2021-08-07 03:17:17 +0530

    Add files via upload

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -12,13 +12,11 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    var leftDiceNumber = 1;
-    var rightDiceNumber = 5;
+    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne") ,  #imageLiteral(resourceName: "DiceTwo") ,  #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix")  ][leftDiceNumber]
-        leftDiceNumber = (leftDiceNumber + 1)%6
-        diceImageView2.image = [ #imageLiteral(resourceName: "DiceOne") ,  #imageLiteral(resourceName: "DiceTwo") ,  #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix")  ][rightDiceNumber]
-        rightDiceNumber = (rightDiceNumber-1+6)%6
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne") ,  #imageLiteral(resourceName: "DiceTwo") ,  #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix")  ]
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
     }
     override func viewDidLoad() {
         super.viewDidLoad()
